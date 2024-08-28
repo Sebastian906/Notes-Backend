a17b46546341cad8d324958bb0fcb0aab484e169
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-28 09:39:12 -0500

    Cambios hacia la conexión de MongoDB

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ require("dotenv").config();
 const config = require("./config.json");
 const mongoose = require("mongoose");
 
-mongoose.connect(config.connectionString);
+//mongoose.connect(config.connectionString);
+
+const connectionString = process.env.MONGODB_CONNECTION
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Conectado a MongoDB');
+}).catch((error) => {
+    console.error('Error conectandose a MongoDB:', error);
+});
 
 const express = require("express");
 const cors = require("cors");
