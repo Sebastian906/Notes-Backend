e358a1ef5f3d2d8bb94737a7d512153a30865407
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-09-01 14:19:39 -0500

    Se agrega la API para buscar notas

--- index.js
+++ index.js
@@ -303,6 +303,41 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
     }
 });
 
+// Buscar Notas
+
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+    const { query } = req.query;
+    
+    if (!query) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Se requiere agregar una busqueda." });
+    }
+
+    try {
+        const matchingNotes = await Notas.find({
+            userId: user._id,
+            $or: [
+                { title: { $regex: new RegExp(query, "i" ) } },
+                { content: { $regex: new RegExp(query, "i" ) } },
+            ],
+        });
+
+        return res.json({
+            error: false,
+            notes: matchingNotes,
+            message: "Se han encontrado las siguientes notas.",
+        });
+
+    } catch(error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error",
+        });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
